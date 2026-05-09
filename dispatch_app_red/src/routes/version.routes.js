c499f091e1a3e02813870e5929946e653a9ef113
sam <sam0931094121@gmail.com>
2026-05-10 02:08:13 +0800

    feat: add version route to provide application version, LAN IP, and port information

--- dispatch_app_red/src/routes/version.routes.js
+++ dispatch_app_red/src/routes/version.routes.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const os = require('os');
 const { appConfig } = require('../config/appConfig');
 
+function getLanIp() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
 router.get('/', (req, res) => {
-  res.json({ version: appConfig.version });
+  res.json({ 
+    version: appConfig.version,
+    lanIp: getLanIp(),
+    port: appConfig.port || 3001
+  });
 });
 
 module.exports = router;
